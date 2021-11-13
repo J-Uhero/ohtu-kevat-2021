21ad3c922db9fade61463665e4042927824d3556
J-Uhero <juho.herranen@gmail.com>
2021-11-13 17:15:20 +0200

    project-reader-tehtävän muutokset

--- viikko2/project-reader/src/index.py
+++ viikko2/project-reader/src/index.py
@@ -1,8 +1,10 @@
 from project_reader import ProjectReader
+import ssl
 
 
 def main():
     url = "https://raw.githubusercontent.com/ohjelmistotuotanto-hy-avoin/python-kevat-2021/main/koodi/viikko3/web-login-robot/pyproject.toml"
+    ssl._create_default_https_context = ssl._create_unverified_context
     reader = ProjectReader(url)
     print(reader.get_project())
 
--- viikko2/project-reader/src/project_reader.py
+++ viikko2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
 from urllib import request
 from project import Project
-
+import toml
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,10 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
+        parced_toml = toml.loads(content)
+        #print(content)
+        poetry = parced_toml["tool"]["poetry"]
 
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(poetry["name"], poetry["description"],
+                       poetry["dependencies"], poetry["dev-dependencies"])
