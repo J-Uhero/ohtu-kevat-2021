4fa13512f263120a579ce870b94e79f3145ab6ac
J-Uhero <juho.herranen@gmail.com>
2021-11-29 00:50:07 +0200

    t. 5.4 testi tehty ja menee läpi

--- viikko4/ttd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/ttd-ostoskori/src/tests/ostoskori_test.py
@@ -5,7 +5,8 @@ from tuote import Tuote
 class TestOstoskori(unittest.TestCase):
     def setUp(self):
         self.kori = Ostoskori()
-        self.tuote1 = Tuote("nimi", 2)
+        self.tuote1 = Tuote("eka", 2)
+        self.tuote2 = Tuote("toka", 3)
 
     def test_ostoskorin_hinta_ja_tavaroiden_maara_alussa(self):
         self.assertEqual(self.kori.hinta(), 0)
@@ -18,3 +19,9 @@ class TestOstoskori(unittest.TestCase):
     def test_korin_hinta_sama_kuin_yhden_tuotteen_hinta(self):
         self.kori.lisaa_tuote(self.tuote1)
         self.assertEqual(self.kori.hinta(), 2)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_tavaraa(self):
+        self.kori.lisaa_tuote(self.tuote1)
+        self.kori.lisaa_tuote(self.tuote2)
+        self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
