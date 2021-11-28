3607a2536c230e89e5d30a27637febc55d0b6f29
J-Uhero <juho.herranen@gmail.com>
2021-11-29 01:44:29 +0200

    t. 5.12 - testi tehty

--- viikko4/ttd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/ttd-ostoskori/src/tests/ostoskori_test.py
@@ -67,3 +67,10 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "ES")
         self.assertEqual(ostos.lukumaara(), 2)
+
+    def test_toinen_korin_kahdesta_samasta_tuotteesta_poistetaan_niin_lkm_yksi(self):
+        self.kori.lisaa_tuote(self.tuote1)
+        self.kori.lisaa_tuote(self.tuote1)
+        self.kori.poista_tuote(self.tuote1)
+        ostos = self.kori.ostokset()[0]
+        self.assertEqual(ostos.lukumaara(), 1)
