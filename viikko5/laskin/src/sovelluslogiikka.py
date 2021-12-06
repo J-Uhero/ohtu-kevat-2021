2566c15ea18ff667c5347c53fb5b2e97026e749c
J-Uhero <juho.herranen@helsinki.fi>
2021-12-06 20:08:09 +0200

    v5 t4-5 - laskin (äskeisestä commitista jäi vahingossa pois tehtävän 4 sisältö eli laskimen toiminnallisuus ilman kumoamista)

---
+++ viikko5/laskin/src/sovelluslogiikka.py
@@ -0,0 +1,62 @@
+class Sovelluslogiikka:
+    def __init__(self, tulos=0):
+        self._tulos = tulos
+        self._edellinen_tulos = 0
+
+    @property
+    def tulos(self):
+        return self._tulos
+    
+    @tulos.setter
+    def tulos(self, arvo):
+        self._edellinen_tulos = self._tulos
+        self._tulos = int(arvo)
+    
+    def kumoa(self):
+        self._tulos = self._edellinen_tulos
+
+class Erotus:
+    def __init__(self, sovellus):
+        self._sovellus = sovellus
+    
+    def suorita(self, arvo):
+        self._sovellus.tulos -= arvo
+
+class Summa:
+    def __init__(self, sovellus):
+        self._sovellus = sovellus
+    
+    def suorita(self, arvo):
+        self._sovellus.tulos += arvo
+
+class Nollaus:
+    def __init__(self, sovellus):
+        self._sovellus = sovellus
+
+    def suorita(self, arvo=0):
+        self._sovellus.tulos = 0
+
+class Kumoa:
+    pass
+    def __init__(self, sovellus):
+        self._sovellus = sovellus
+
+    def suorita(self, arvo):
+        self._sovellus.kumoa()
+
+    '''
+    def __init__(self, tulos=0):
+        self.tulos = tulos
+
+    def miinus(self, arvo):
+        self.tulos = self.tulos - arvo
+
+    def plus(self, arvo):
+        self.tulos = self.tulos + arvo
+
+    def nollaa(self):
+        self.tulos = 0
+
+    def aseta_arvo(self, arvo):
+        self.tulos = arvo
+        '''
